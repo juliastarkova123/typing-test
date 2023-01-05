086edcfe8435904b1b7a79c9525465a2987fd0e0
Julia Starkova <juliastarkova123@rambler.ru>
2023-01-05 22:15:41 +0700

    Task#16478: Add language toggle layout

--- typing-test/src/components/main-wrapper/MainWrapper.js
+++ typing-test/src/components/main-wrapper/MainWrapper.js
@@ -1,11 +1,13 @@
 import styles from './MainWrapper.module.css'
 import Header from '../header/Header';
+import LanguageToggler from "../test-language-toggler/LanguageToggler";
 
 function MainWrapper() {
 
     return (
         <div className={styles.main_wrapper}>
             <Header />
+            <LanguageToggler />
         </div>
     )
 }
