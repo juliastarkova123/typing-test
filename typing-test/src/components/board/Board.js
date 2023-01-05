0951ed1ee41b011bbaa6cb5b51927f6b6e80378a
Julia Starkova <juliastarkova123@rambler.ru>
2023-01-05 23:49:42 +0700

    Task#16475: Add base algorithm of filling lines in the words board
    It calculates length of line and every word inside

--- typing-test/src/components/board/Board.js
+++ typing-test/src/components/board/Board.js
@@ -1,12 +1,35 @@
 import styles from './Board.module.css'
 
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 function Board() {
 
+    const firstLine = useRef(null)
+    const secondLine = useRef(null)
+
+    useEffect(() => {
+        fillLine(0)
+        fillLine(1)
+    })
+
+    const fillLine = (lineNumber) => {
+        const line = lineNumber === 0 ? firstLine : secondLine
+        const width = line.current.clientWidth - 18 * 2    // subtract left and right paddings
+        let widthOfSpans = 0
+        for (let i = 0; i < line.current.children.length; i++) {
+            widthOfSpans += line.current.children[i].clientWidth
+        }
+        if (widthOfSpans < width) {
+            // add word
+        }
+        else if (widthOfSpans >= width) {
+            // remove last word
+        }
+    }
+
     return (
         <div className={styles.board}>
-            <div className={styles.board_line}>
+            <div className={styles.board_line} ref={firstLine}>
                 <span className={styles.board_word}>
                     <span>w</span>
                     <span>o</span>
@@ -26,7 +49,7 @@ function Board() {
                     <span>d</span>
                 </span>
             </div>
-            <div className={styles.board_line}>
+            <div className={styles.board_line} ref={secondLine}>
                 <span className={styles.board_word}>
                     <span>w</span>
                     <span>o</span>
