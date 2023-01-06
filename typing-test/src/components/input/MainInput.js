4b347e4ace51d3f25433dbe39170869dac05a97c
Julia Starkova <juliastarkova123@rambler.ru>
2023-01-06 18:46:54 +0700

    Task#16481: Use reset action inside main input

--- typing-test/src/components/input/MainInput.js
+++ typing-test/src/components/input/MainInput.js
@@ -1,28 +1,33 @@
 import styles from './MainInput.module.css'
 
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { resetBoard } from '../../global-state/reducers/boardReducer';
 
 import Timer from "../timer/Timer";
 
 function MainInput() {
 
-    const [curValue, setCurValue] = useState("")
+  const [curValue, setCurValue] = useState("")
 
-    const handleChange = (event) => {
-        setCurValue(event.target.value)
-    }
+  const dispatch = useDispatch()
 
-    const onClickRestart = () => {
-        setCurValue("")
-    }
+  const handleChange = (event) => {
+    setCurValue(event.target.value)
+  }
 
-    return (
-        <div className={styles.main_input_wrapper}>
-            <input type={"text"} className={styles.main_input} value={curValue} onChange={handleChange} />
-            <span className={styles.time_span}><Timer /></span>
-            <button className={styles.btn_redo} onClick={onClickRestart}><i className="fas fa-redo fa-1x"></i></button>
-        </div>
-    )
+  const onClickRestart = () => {
+    setCurValue("")
+    dispatch(resetBoard())
+  }
+
+  return (
+      <div className={styles.main_input_wrapper}>
+          <input type={"text"} className={styles.main_input} value={curValue} onChange={handleChange} />
+          <span className={styles.time_span}><Timer /></span>
+          <button className={styles.btn_redo} onClick={onClickRestart}><i className="fas fa-redo fa-1x"></i></button>
+      </div>
+  )
 }
 
 export default MainInput;
